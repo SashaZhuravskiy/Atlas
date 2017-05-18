6a262eea44b4d34f41dcf0607af50313a4f2104a
Alex <sasha_zhuravskiy@mail.ru>
2017-05-18 22:32:04 +0300

    Third commit

--- app/src/main/java/com/example/admin/vocabulary1/Country.java
+++ app/src/main/java/com/example/admin/vocabulary1/Country.java
@@ -15,7 +15,7 @@ public class Country {
         this.capital = translate;
         this.id = id;
     }
-    //привет
+    //привет Женя
     public String GetCountry(){return country;}
     public String GetCapital(){return capital;}
     public long getId() { return id;}
